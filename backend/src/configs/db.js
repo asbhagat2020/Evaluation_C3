30811dd7ade2c182f747d6abfa0b2e5c19ece377
Ashwin <asbhagat2020@gmail.com>
2024-07-08 22:16:09 +0530

    add

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,8 +1,29 @@
 const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 
-const sequelize = new Sequelize('nem101', 'root', '002266', {
-    host: 'localhost',
-    dialect: 'mysql'
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,  // Use default port if not specified
+    logging: console.log,
 });
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connected...');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err.message);
+    });
+
+sequelize.sync({ alter: true })
+    .then(() => {
+        console.log('Database synchronized');
+    })
+    .catch((error) => {
+        console.error('Error synchronizing database:', error.message);
+    });
 
 module.exports = sequelize;
